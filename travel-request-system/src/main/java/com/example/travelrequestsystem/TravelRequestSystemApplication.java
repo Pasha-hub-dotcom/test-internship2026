58b46d304436e451ccedcb9d70b48cf7f3e86a5f
pavel <pavel_daroshka@senla.eu>
2026-09-16 11:44:26 +0300

    Add Spring Boot MVC module: travel-request-system

---
+++ travel-request-system/src/main/java/com/example/travelrequestsystem/TravelRequestSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelrequestsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelRequestSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelRequestSystemApplication.class, args);
+    }
+
+}
